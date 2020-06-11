dab0a754b349a7bbecba1ef3adb863998f020fc5
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-11 13:13:47 -0700

    Setup Base Product class

--- app.py
+++ app.py
@@ -16,6 +16,19 @@ app.config['SQLALCHEMY_TRACK_MODIFCAITONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class Product(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), unique=True)
+	description = db.Column(db.String(200))
+	price = db.Column(db.Float)
+	qty = db.Column(db. Integer)
+
+	def __init__(self, name, description, price, qty):
+		self.name = name
+		self.description = description
+		self.price = price
+		self.qty = qty 
+
 
 
 # @app.route('/', methods=['GET'])
