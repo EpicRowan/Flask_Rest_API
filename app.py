5e5782ade8aa20caea13a8177632747831cab505
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-11 13:09:52 -0700

    SQlalchemy and Marshmallow initialized

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ import os
 
 app = Flask(__name__)
 
+# make sure we can correcly locate the db file
+basedir =os.path.abspath(os.path.dirname(__file__))
+
+# will look for a db called sqlite in current folder
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFCAITONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+
+# @app.route('/', methods=['GET'])
+# def get():
+# 	return jsonify({'msg': "Hello!!!"})
+
 app.secret_key = "supersecret"
 
 if __name__ == '__main__':
